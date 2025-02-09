655a3b276d9163ab03db36259bba2d3bbaf67035
turdal1hasanboyev <turdalihasanboyev70@gmail.com>
2025-02-09 18:00:47 +0500

    Writing about and profile pages!

--- apps/account/views.py
+++ apps/account/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from django.contrib import messages
+
+from django.views import View
+
+from apps.base.models import SubEmail
+
+
+class ProfileView(View):
+    """
+    View to display user's profile information.
+    """
+
+    template_name = 'profile.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+
+    def post(self, request, *args, **kwargs):
+        sub_email = request.POST.get("sub_email")
+
+        if SubEmail.objects.filter(email=sub_email).exists():
+            messages.error(request, "Email already exists")
+            return redirect('profile')
+
+        if sub_email:
+            SubEmail.objects.create(email=sub_email)
+            messages.success(
+                request, 'Your email has been successfully subscribed.')
+            return redirect('profile')
+        else:
+            messages.error(request, 'Please enter your email address.')
+            return redirect('profile')
--- apps/base/views.py
+++ apps/base/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from django.contrib import messages
+
+from django.views import View
+
+from ..base.models import SubEmail
+
+
+class AboutView(View):
+    """
+    View for the about page.
+    """
+
+    def get(self, request, *args, **kwargs):
+        return render(request, 'about.html')
+    
+    def post(self, request, *args, **kwargs):
+        sub_email = request.POST.get('sub_email')
+
+        if SubEmail.objects.filter(email=sub_email).exists():
+            messages.error(request, 'Email already subscribed')
+            return redirect(request.META.get('HTTP_REFERER'))
+
+        if sub_email:
+            SubEmail.objects.create(email=sub_email)
+            messages.success(request, 'Email subscribed')
+            return redirect(request.META.get('HTTP_REFERER'))
+        else:
+            messages.error(request, 'Please enter your email address.')
+            return redirect(request.META.get('HTTP_REFERER'))
