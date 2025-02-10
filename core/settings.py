44c9af896c11cc1b9ec27bdd7798c20f92bd5e3c
turdal1hasanboyev <turdalihasanboyev70@gmail.com>
2025-02-10 19:12:11 +0500

    Upgrading messages in html and finish project!!

--- core/settings.py
+++ core/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env.str("SECRET_KEY", default='SECRET_KEY')  # SECRET_KEY
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True  # DEBUG
+DEBUG = False  # DEBUG
 
 ALLOWED_HOSTS = ['*']  # ALLOWED_HOSTS
 
