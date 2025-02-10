04cf7842399fca10431ebab13da0862979220f4a
turdal1hasanboyev <turdalihasanboyev70@gmail.com>
2025-02-10 18:28:19 +0500

    Writing login page

---
+++ apps/account/login.py
@@ -0,0 +1,32 @@
+from django.views import View
+
+from django.shortcuts import render, redirect
+
+from django.contrib.auth import authenticate, login
+
+from django.contrib import messages
+
+
+class LogInView(View):
+    """
+    View for handling user login.
+    """
+
+    template_name = 'login.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+    
+    def post(self, request, *args, **kwargs):
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        user = authenticate(request, email=email, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'You have successfully logged in.')
+            return redirect('profile')
+        else:
+            messages.error(request, 'Invalid email or password.')
+            return redirect('login')
--- apps/account/urls.py
+++ apps/account/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 
 from .views import ProfileView
 from .logout import LogOutView
+from .login import LogInView
 
 
 urlpatterns = [
     path('profile/', ProfileView.as_view(), name='profile'),
     path('logout/', LogOutView.as_view(), name='logout'),
+    path('login/', LogInView.as_view(), name='login'),
 ]
